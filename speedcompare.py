a479d75a332721f961dff369327f3f20e7b50344
Christopher Xu <christopher.y.x@gmail.com>
2023-08-07 01:49:11 -0500

    interactive slider

--- speedcompare.py
+++ speedcompare.py
@@ -22,10 +22,8 @@ def jac_angles(vars):
         [-L2*sin(theta2), -L3*sin(theta3)]
     ]
 
-def func_sincos(vars, theta1):
+def func_sincos(vars):
     sin2, cos2, sin3, cos3 = vars
-    sin1 = sin(theta1)
-    cos1 = cos(theta1)
     return [
         L1*sin1 + L2*sin2 + L3*sin3,
         L1*cos1 + L2*cos2 + L3*cos3 - L4, 
@@ -33,7 +31,7 @@ def func_sincos(vars, theta1):
         sin3**2 + cos3**2 - 1,        
     ]
 
-def jac_sincos(vars, theta1):
+def jac_sincos(vars):
     sin2, cos2, sin3, cos3 = vars
     return [
         [L2, 0, L3, 0],
@@ -44,9 +42,8 @@ def jac_sincos(vars, theta1):
 
 
 # Generate random initial guesses
-guess = np.array([1, 0])
-guess4 = np.array([sin(guess[0]), cos(guess[0]), sin(guess[1]), cos(guess[1])])
-
+guess2 = np.array([1, 5])
+guess4 = np.array([sin(guess2[0]), cos(guess2[0]), sin(guess2[1]), cos(guess2[1])])
 
 
 time1, time2 = (0, 0)
@@ -71,22 +68,25 @@ def jac_sincos(vars):
         [0, 0, 2*sin3, 2*cos3]
     ]
 
+ntests = 4
+times = np.zeros(ntests)
+
 n = 1000
 for i in range(n):
 
 
     start_time = time.time()
-
-    solution1 = root(func_sincos, jac=jac_sincos, x0=guess4, method='hybr')
-    # solution1 = np.mod(solution1.x, 2*pi)
-    solution1 = solution1.x
-    time1 += time.time() - start_time
-
+    sol0 = root(func_sincos, jac=jac_sincos, x0=guess4, method='hybr').x
+    times[0] += time.time() - start_time
 
     start_time = time.time()
-    x0 = guess
+    sol1 = root(func_angles, jac=jac_angles, x0=guess2).x
+    times[1] += time.time() - start_time
+
+    start_time = time.time()
     tolerance = 1e-6
     max_iterations = 100
+    x0 = guess2
     for iteration in range(max_iterations):
         theta2, theta3 = x0
         h = [
@@ -94,21 +94,32 @@ for i in range(n):
             (-L1*cos(theta1-theta2) - L2 - L3*cos(theta2-theta3) + L4*cos(theta2)) / (L3*(theta2-theta3))
         ]
         x_new = x0 + h
-
-        # print(x_new)
-
         if np.linalg.norm(x_new - x0) < tolerance:
             break
-
         x0 = x_new
-    solution2 = x0
-    time2 += time.time() - start_time
+    sol2 = x0
+    times[2] += time.time() - start_time
+
+    start_time = time.time()
+    tolerance = 1e-6
+    max_iterations = 100
+    x0 = guess4
+    for iteration in range(max_iterations):
+        sin2, cos2, sin3, cos3 = x0
+        h = [
+            (L2*cos3*(cos2**2 + sin2**2 - 1) + L3*cos2*(cos3**2 + sin3**2 - 1) - 2*cos2*(cos3*(L1*cos1 + L2*cos2 + L3*cos3 - L4) + sin3*(L1*sin1 + L2*sin2 + L3*sin3)))/(2*L2*(cos2*sin3 - cos3*sin2)),
+            (-L2*sin3*(cos2**2 + sin2**2 - 1) - L3*sin2*(cos3**2 + sin3**2 - 1) + 2*sin2*(cos3*(L1*cos1 + L2*cos2 + L3*cos3 - L4) + sin3*(L1*sin1 + L2*sin2 + L3*sin3)))/(2*L2*(cos2*sin3 - cos3*sin2)),
+            (-L2*cos3*(cos2**2 + sin2**2 - 1) - L3*cos2*(cos3**2 + sin3**2 - 1) + 2*cos3*(cos2*(L1*cos1 + L2*cos2 + L3*cos3 - L4) + sin2*(L1*sin1 + L2*sin2 + L3*sin3)))/(2*L3*(cos2*sin3 - cos3*sin2)),
+            (L2*sin3*(cos2**2 + sin2**2 - 1) + L3*sin2*(cos3**2 + sin3**2 - 1) - 2*sin3*(cos2*(L1*cos1 + L2*cos2 + L3*cos3 - L4) + sin2*(L1*sin1 + L2*sin2 + L3*sin3)))/(2*L3*(cos2*sin3 - cos3*sin2))
+        ]
+        x_new = x0 + h
+        if np.linalg.norm(x_new - x0) < tolerance:
+            break
+        x0 = x_new
+    sol3 = x0
+    times[3] += time.time() - start_time
 
 
 
-
-
-print("1:    ", solution1, time1*1000/n)
-print("2:    ", [sin(solution2[0]), cos(solution2[0]), sin(solution2[1]), cos(solution2[1])], time2*1000/n)
-
-
+for i in range(ntests):
+    print(i, round(times[i], 5))
