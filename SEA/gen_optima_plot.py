dd406d6f9a0b56e0a7a36949719c8dddb05d8cee
Christopher Xu <christopher.y.x@gmail.com>
2023-11-09 23:17:19 -0600

    regen visuals and compression spring data

--- SEA/gen_optima_plot.py
+++ SEA/gen_optima_plot.py
@@ -10,6 +10,9 @@ import itertools
 topN = 5
 nsteps = 20
 
+tau_max=0.05
+omega_max=3000
+
 filedir = os.path.dirname(os.path.abspath(__file__))
 exp_name = "exp_2023-10-29-12-10-02"
 exp_path = f"{filedir}/{exp_name}"
@@ -44,25 +47,58 @@ for param_path in param_folders:
         file = file[:-4] #remove '.txt'
         v_file = round(int(file.split("_")[1].split("v")[1]) * 0.001, 3)
         
+        
+
+        ts = np.linspace(0, t_total, nsteps+1)
+        t = t_total/nsteps
+
+        x_as = X_optload[0,:]
+        x_ls_adj = X_optload[1,:]
+        v_ls = X_optload[2,:]
+        T1s = X_optload[3,:]
+        T2s = X_optload[4,:]
+
+        v_file = v_ls[-1]
         if(v_file > v_max):
             v_max = v_file
             X_best = X_optload
 
-        ts = np.linspace(0, t_total, nsteps+1)
-        axs[0].step(ts, X_optload[2], 'r', alpha=0.2)
-        # ax0_twin.step(ts, X_optload[4], 'b', alpha=0.2)
+        KE = (1/2)*m_l*v_ls**2
+        PE = (1/2)*k_test*(x_as*T1s - x_ls_adj/T2s)**2
 
-    axs[0].step(ts, X_best[2], 'r--', label=f"best", alpha=0.8, linewidth=2)
+        # axs[0].step(ts, X_optload[2], 'r', alpha=0.2)
+
+        axs[0].plot(ts, KE, 'blue', alpha=0.3)
+        # axs[0].plot(ts, PE, 'orange', alpha=0.3)
+        # axs[0].plot(ts, E_motor, 'red', alpha=0.2)
+        axs[0].plot(ts, PE, 'orange', alpha=0.3)
+
+    x_as = X_best[0,:]
+    x_ls_adj = X_best[1,:]
+    v_ls = X_best[2,:]
+    T1s = X_best[3,:]
+    T2s = X_best[4,:]
+
+    KE_best = (1/2)*m_l*v_ls**2
+    PE_best = (1/2)*k_test*(x_as*T1s - x_ls_adj/T2s)**2
+
+    axs[0].plot(ts, KE_best,  linestyle='--', label=f"kinetic of best", alpha=0.8, linewidth=2)
+    axs[0].plot(ts, PE_best,  linestyle='--', label=f"potential of best", alpha=0.8, linewidth=2)
     # ax0_twin.step(ts, X_best[4], 'b--', label=f"best", alpha=0.8, linewidth=2)
     # ax0_twin.set_ylim(0, 20)
     axs[0].set_xlabel('Time (seconds)')
-    axs[0].set_ylabel('Leg velociy (m/s)')
+    axs[0].set_ylabel('Energy (J)')
     # ax0_twin.set_ylabel('T2', color='blue')
 
-    plt.title(f"Leg velocity for top {topN} transmission profiles\nk={k_test}, m={m_l}, t={t_total}, L={leg_max}, v_best={v_max}")
+    axs[0].legend(loc='upper left')
+    # ax0_twin.legend(loc='lower right')
+
+    plt.title(f"Kinetic and potential energy for top {topN} transmission profiles\nk={k_test}, m={m_l}, t={t_total}, L={leg_max}, v_best={round(v_max,3)}")
 
     os.makedirs(f"{exp_path}/figs_legvel", exist_ok=True)
     plt.savefig(f'{exp_path}/figs_legvel/{param_str}.png', dpi=300)
     plt.close()
+
+    # break
 
 
